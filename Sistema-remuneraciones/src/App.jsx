31359a39242e4140a0454390ca01a48e2088e494
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-06-22 22:41:10 -0500

    Edicion en front

--- Sistema-remuneraciones/src/App.jsx
+++ Sistema-remuneraciones/src/App.jsx
@@ -1,7 +1,9 @@
+//App.jsx
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './Welcome';
 import Login from './Login';
+import DashboardSummary from './DashboardSummary';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardSummary />} /> {/* nueva ruta */}
+        
       </Routes>
     </BrowserRouter>
   );
